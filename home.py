0fcd23ccb7db0a5c4e1f0b48cbb9a9fe163619b2
Shanemel Asuncion <shanemelasuncion@gmail.com>
2025-02-12 15:44:08 -0700

    ai-chatbot

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Welcome to my first AI Chatbot")
+
+st.divider()
+
+st.write("This is a simple chatbot built using Streamlit. Interact with me!")
+
+st.image("https://cdn.pixabay.com/photo/2023/03/11/22/12/ai-generated-7845409_960_720.jpg")
+
+st.subheader("Build with love @ShanemelAsuncion <3")
